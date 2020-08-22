1f22a792448f47590683470ac110837d53d50cde
jeisonbastos <jeison.bastos@hotmail.com>
2020-08-22 00:16:17 -0600

    Update ShowController

--- app/Http/Controllers/ShowController.php
+++ app/Http/Controllers/ShowController.php
@@ -21,7 +21,7 @@ class ShowController extends Controller
     public function index()
     {
         try {
-            $funciones = Show::where('visible_cartelera', true)->orderBy('location_id', 'desc')->orderBy('movie_id', 'desc')->with(['movie', 'tickets'])->get();
+            $funciones = Show::where('visible_cartelera', true)->orderBy('location_id', 'desc')->orderBy('movie_id', 'desc')->with(['movie', 'tickets', 'location'])->get();
             $response = $funciones;
 
             return response()->json($response, 200);
@@ -39,7 +39,7 @@ class ShowController extends Controller
     public function sell()
     {
         try {
-            $funciones = Show::where('disponible_venta', true)->orderBy('location_id', 'desc')->orderBy('movie_id', 'desc')->withCount(['movie', 'tickets'])->get();
+            $funciones = Show::where('disponible_venta', true)->orderBy('location_id', 'desc')->orderBy('movie_id', 'desc')->withCount(['movie','tickets', 'location'])->get();
             $response = $funciones;
 
             return response()->json($response, 200);
@@ -55,7 +55,7 @@ class ShowController extends Controller
     public function all()
     {
         try {
-            $funciones = Show::orderBy('location_id', 'desc')->orderBy('movie_id', 'desc')->with(['movie', 'tickets'])->get();
+            $funciones = Show::orderBy('location_id', 'desc')->orderBy('movie_id', 'desc')->with(['movie','tickets', 'location'])->get();
             $response = $funciones;
 
             return response()->json($response, 200);
@@ -72,7 +72,7 @@ class ShowController extends Controller
     public function show($id)
     {
         try {
-            $funcion = Show::where('id', $id)->orderBy('location_id', 'desc')->orderBy('movie_id', 'desc')->with(['movie', 'tickets'])->get();
+            $funcion = Show::where('id', $id)->orderBy('location_id', 'desc')->orderBy('movie_id', 'desc')->with(['movie','tickets', 'location'])->get();
             $response = $funcion;
 
             return response()->json($response, 200);
@@ -89,7 +89,7 @@ class ShowController extends Controller
     public function show_for_location($location_id)
     {
         try {
-            $funcion = Show::where('location_id', $location_id)->orderBy('location_id', 'desc')->orderBy('movie_id', 'desc')->with(['movie', 'tickets'])->get();
+            $funcion = Show::where('location_id', $location_id)->orderBy('location_id', 'desc')->orderBy('movie_id', 'desc')->with(['movie','tickets', 'location'])->get();
             $response = [$funcion];
 
             return response()->json($response, 200);
@@ -106,7 +106,7 @@ class ShowController extends Controller
     public function show_for_movie($movie_id)
     {
         try {
-            $funcion = Show::where('movie_id', $movie_id)->orderBy('location_id', 'desc')->orderBy('movie_id', 'desc')->with(['movie', 'tickets'])->get();
+            $funcion = Show::where('movie_id', $movie_id)->orderBy('location_id', 'desc')->orderBy('movie_id', 'desc')->with(['movie','tickets', 'location'])->get();
             $response = [$funcion];
 
             return response()->json($response, 200);
